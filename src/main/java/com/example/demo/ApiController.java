14f510f2435bb9d3a00a633058b5f43e85341eb3
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2024-02-12 12:11:24 +0000

    Add vulnerability

---
+++ src/main/java/com/example/demo/ApiController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+
+@RestController
+public class ApiController {
+
+  @GetMapping(value = "/get/{username}/{password}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<String> getById(@PathVariable String username, @PathVariable String password) {
+
+    MockSqlConnection mockSqlConnection = new MockSqlConnection();
+    mockSqlConnection.sqlConnection(username, password);
+    
+    String response = "SQL attempt made";
+
+    return ResponseEntity.ok().body(response);
+  }
+  
+}
---
+++ src/main/java/com/example/demo/MockSqlConnection.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MockSqlConnection {
+
+	public MockSqlConnection() {};
+
+  public void sqlConnection(String username, String password) {
+    String jdbcUrl = "someJDBCURL";
+
+    try {
+      Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+      String sql = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+      Statement statement = connection.createStatement();
+      ResultSet rs = statement.executeQuery(sql);
+
+      String result;
+      if (rs.first()) {
+        result = "found some stuff";
+      } else {
+        result = "found nothing";
+      }
+
+      System.out.println(result);
+
+      connection.close();
+
+    } catch (SQLException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+}
